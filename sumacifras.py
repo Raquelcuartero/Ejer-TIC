ea381a81d35e1acb2fe0b68fc74f891d5a83f0cd
Raquelcuartero <33655363+Raquelcuartero@users.noreply.github.com>
2017-11-21 11:33:03 +0100

    Delete sumacifras.py

--- sumacifras.py
+++
@@ -1,7 +0,0 @@
-def suma_cifras():
-    numero=input("Dime un numero:")
-    decenas=numero/10
-    unidades=numero%10
-    print "La suma de las cifras de",numero,"vale",decenas+unidades
-suma_cifras()
-    
